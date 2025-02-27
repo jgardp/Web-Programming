e4385bf923e737f0fd7bec8dafddf53610bdf88e
jgardp <jgardunopena@atu.edu>
2025-02-26 21:07:38 -0600

    Updated app.js with function call and css

--- Day 13/react-three-cards/src/Card.js
+++ Day 13/react-three-cards/src/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Card.css"; //Import CSS for styling
 
-function Card(title, content, color){
+function Card({title, content, color}){
     return (
         <div className="card" style={{backgroundColor: color}}>
             <h2>{title}</h2>
