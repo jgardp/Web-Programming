b15bf7c7ce4b47785202d103ddea00751a2d7b5a
jgardp <jgardunopena@atu.edu>
2025-03-10 22:07:18 -0500

    comment change

--- Day 17/my-express-app/server.js
+++ Day 17/my-express-app/server.js
@@ -44,7 +44,7 @@ const vehicles = [
     {id:4, make:"Tesla", model: "Cybertruck"},
 ]
 
-//Routing... req = request from client   res = response from server
+//Routing... 
 app.get("/", (req, res) => res.send("Welcome to my API!"));
 
 app.get("/users", (req, res) => res.json(users));
