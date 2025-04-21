afb4e7fba43d33d776d92dfcda96d64957b47ab8
jgardp <jgardunopena@atu.edu>
2025-04-20 20:22:40 -0500

    final project frontend and backend frame/folders created

---
+++ Final Project/final-backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Connect to MongoDB
+
+//Define schema and model
+
+//Routes
+
+//Start server!
