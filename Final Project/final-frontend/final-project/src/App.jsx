f39fd1f413a2aaae1f969bcedb5d45308a6d6016
jgardp <jgardunopena@atu.edu>
2025-05-03 23:05:38 -0500

    TailwindCSS setup and basic home page created

--- Final Project/final-frontend/final-project/src/App.jsx
+++ Final Project/final-frontend/final-project/src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
 import './App.css'
+import profile from './assets/profile.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="justify-center flex min-h-screen bg-gray-100 flex-col items-center p-5 font-mono">
+              <header className="text-center m-5">
+                <h1 className="font-bold text-gray-900 text-4xl">Jose Isidro Garduno Pena</h1>
+              </header>
+              <img className="w-45 h-45 rounded-full mb-2" src={profile} alt="profile image"></img>
+              <main>
+                <div className="max-w-3xl bg-white m-5 rounded-2xl shadow-md text-center p-5">
+                  <p className="text-gray-700 mb-2">Bio: 24 year old student currently studying Information Technology at Arkansas Tech University.</p>
+                </div>
+      
+                <div className="max-w-3xl bg-white m-5 rounded-2xl shadow-md text-center p-5">
+                  <p className="text-gray-700">Email: <a href="#" className="hover:underline">isrogar8@gmail.com</a></p>
+                </div>
+              </main>
+              <footer className="w-full text-center mt-1 mb-2">
+              </footer>
+            </div>
     </>
   )
 }
