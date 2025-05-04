7c130bdf7418a1757a98c0e17d8ee55c53c2dd66
jgardp <jgardunopena@atu.edu>
2025-05-03 21:57:13 -0500

    MongoDB backend schemas created

--- Final Project/final-backend/server.js
+++ Final Project/final-backend/server.js
@@ -9,10 +9,50 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-//Connect to MongoDB
+//Connecting to MongoDB
+mongoose.connect('mongodb://localhost:27017/finalDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=> console.log('MongoDB Connected'))
+.catch((err)=>console.error('MongoDB error', err));
 
-//Define schema and model
+//Defining schemas and models
+const projectsSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    technologies: String,
+    link: String,
+    image: String,
+})
+const Projects = mongoose.model('Projects', projectsSchema);
+
+const educationSchema = new mongoose.Schema({
+    institution: String,
+    degree: String,
+    year: String,
+    description: String,
+})
+const Education = mongoose.model('Education', educationSchema);
+
+const skillsSchema = new mongoose.Schema({
+    name: String,
+    level: String,
+    icon: String,
+})
+const Skills = mongoose.model('Skills', skillsSchema);
+
+const contact_messagesSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    message: String,
+    timestamp: String,
+})
+const Contacts = mongoose.model('Projects', contact_messagesSchema);
 
 //Routes
 
 //Start server!
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
