2b1bdfc3b35a97c787fd501f31d16a4e6eaf1978
jgardp <jgardunopena@atu.edu>
2025-05-03 22:13:52 -0500

    MongoDB data created and backend routes added

--- Final Project/final-backend/server.js
+++ Final Project/final-backend/server.js
@@ -38,7 +38,6 @@ const Education = mongoose.model('Education', educationSchema);
 const skillsSchema = new mongoose.Schema({
     name: String,
     level: String,
-    icon: String,
 })
 const Skills = mongoose.model('Skills', skillsSchema);
 
@@ -48,9 +47,61 @@ const contact_messagesSchema = new mongoose.Schema({
     message: String,
     timestamp: String,
 })
-const Contacts = mongoose.model('Projects', contact_messagesSchema);
+const Contact = mongoose.model('Projects', contact_messagesSchema);
 
 //Routes
+
+// Get all projects data
+app.get('/projects', async (req, res) =>{
+    try{
+        const projects = await Projects.find();
+    }
+    catch(err){
+        res.status(500).json({message: 'Failed to fetch projects.'});
+    }
+});
+
+// Get all education data
+app.get('/education', async (req, res) =>{
+    try{
+        const education = await Education.find();
+    }
+    catch(err){
+        res.status(500).json({message: 'Failed to fetch education.'});
+    }
+});
+
+// Get all skills data
+app.get('/skills', async (req, res) =>{
+    try{
+        const skills = await Skills.find();
+    }
+    catch(err){
+        res.status(500).json({message: 'Failed to fetch skills.'});
+    }
+});
+
+// Get all messages data
+app.get('/contact', async (req, res) =>{
+    try{
+        const contact = await Contact.find();
+    }
+    catch(err){
+        res.status(500).json({message: 'Failed to fetch contact.'});
+    }
+});
+
+// Form submitting messages
+app.post('/contact', async (req, res) => {
+    try{
+        const {name, email, message, timestamp} = req.body;
+        const newMessage = new Contact({name, email, message, timestamp});
+        await newMessage.save();
+        res.json(newMessage);
+    } catch(err){
+        res.status(500).json({message: 'Failed to save message!'});
+    }
+});
 
 //Start server!
 app.listen(PORT, ()=>{
