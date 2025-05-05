b7ee627b7b976b2ae1cc831b7a243beb66033097
jgardp <jgardunopena@atu.edu>
2025-05-04 19:45:06 -0500

    skills page updated with images and data

--- Final Project/final-frontend/final-project/src/Skills.jsx
+++ Final Project/final-frontend/final-project/src/Skills.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+
+import react from './assets/react.png';
+import tailwind from './assets/tailwind.png';
+import mongodb from './assets/mongodb.png';
+import express from './assets/express.png';
+import web from './assets/web.jpg';
 
 function Skills() {
 
-  return (
+  const [skills, setSkills] = useState([]);
+  
+  useEffect(()=>{
+    axios.get('http://localhost:5000/skills')
+    .then(res => setSkills(res.data))
+    .catch(err => console.error("Error fetching skills.", err));
+  }, []);
+
+  return ( 
     <>
-      <div className="justify-center flex min-h-screen bg-neutral-200 flex-col items-center p-5 font-mono">
-        <p>Skills</p>
+      <div className="justify-center flex bg-neutral-200 items-center pt-5 space-x-5">
+        <img className="w-20 h-20 rounded-full mb-2" src={react} alt="logo"></img>
+        <img className="w-20 h-20 rounded-full mb-2" src={tailwind} alt="logo"></img>
+        <img className="w-20 h-20 rounded-full mb-2" src={mongodb} alt="logo"></img>
+        <img className="w-20 h-20 rounded-full mb-2" src={express} alt="logo"></img>
+        <img className="w-20 h-20 rounded-full mb-2" src={web} alt="logo"></img>
+      </div>
+      <div className="justify-center flex min-h-screen bg-neutral-200 flex-col items-center p-3 font-mono">
+        {skills.map(data => (
+            <div key={data.id} className="max-w-3xl bg-white m-5 rounded-2xl shadow-md text-center p-5">
+              <p className="font-bold">Skill: {data.name}</p>
+              <p>Proficiency: {data.level}</p>
+            </div>
+        ))}
       </div>
     </>
   )
