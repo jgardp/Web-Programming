a1fe86451ee8605c531f739911cf09006faea6c5
jgardp <jgardunopena@atu.edu>
2025-05-04 19:38:39 -0500

    education page created and update with database data

--- Final Project/final-frontend/final-project/src/Education.jsx
+++ Final Project/final-frontend/final-project/src/Education.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function Education() {
 
+  const [education, setEducation] = useState([]);
+  
+  useEffect(()=>{
+    axios.get('http://localhost:5000/education')
+    .then(res => setEducation(res.data))
+    .catch(err => console.error("Error fetching education.", err));
+  }, []);
+
   return (
+    
     <>
       <div className="justify-center flex min-h-screen bg-neutral-200 flex-col items-center p-5 font-mono">
-        <p>Education</p>
+        {education.map(data => (
+            <div key={data.id} className="max-w-3xl bg-white m-5 rounded-2xl shadow-md text-center p-5">
+              <p className="font-bold">Degree: {data.degree}</p>
+              <p>Description: {data.description}</p>
+              <p>Institution: {data.institution}</p>
+              <p>Graducation Year: {data.year}</p>
+            </div>
+        ))}
       </div>
     </>
   )
