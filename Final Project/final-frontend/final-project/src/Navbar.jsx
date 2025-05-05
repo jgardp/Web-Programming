8b9b2219c76d7c9325e97ba03a98bbf732f92e2b
jgardp <jgardunopena@atu.edu>
2025-05-05 08:18:53 -0500

    update navbar styling

--- Final Project/final-frontend/final-project/src/Navbar.jsx
+++ Final Project/final-frontend/final-project/src/Navbar.jsx
@@ -4,12 +4,12 @@ import './App.css';
 function Navbar (){
     return (
         <nav>
-            <ul className="flex flex-wrap text-center p-3 justify-center space-x-20 font-mono">
-                <li className="hover:bg-slate-400 p-2 rounded-full hover:text-white"><Link to="/">Home</Link></li>
-                <li className="hover:bg-slate-400 p-2 rounded-full hover:text-white"><Link to="/projects">Projects</Link></li>
-                <li className="hover:bg-slate-400 p-2 rounded-full hover:text-white"><Link to="/education">Education</Link></li>
-                <li className="hover:bg-slate-400 p-2 rounded-full hover:text-white"><Link to="/skills">Skills</Link></li>
-                <li className="hover:bg-slate-400 p-2 rounded-full hover:text-white"><Link to="/contact">Contact</Link></li>
+            <ul className="flex flex-wrap text-center p-3 justify-center space-x-20 font-mono bg-slate-600">
+                <li className="hover:bg-slate-400 p-2 rounded-full text-white"><Link to="/">Home</Link></li>
+                <li className="hover:bg-slate-400 p-2 rounded-full text-white"><Link to="/projects">Projects</Link></li>
+                <li className="hover:bg-slate-400 p-2 rounded-full text-white"><Link to="/education">Education</Link></li>
+                <li className="hover:bg-slate-400 p-2 rounded-full text-white"><Link to="/skills">Skills</Link></li>
+                <li className="hover:bg-slate-400 p-2 rounded-full text-white"><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     )
